3d4aa864fc5709ab9eaba0376efb6ce052eeb3d3
azimust <woody.azimbek@gmail.com>
2023-07-24 14:53:39 +0600

    imrovement: add inst link

--- src/components/Home/Social.jsx
+++ src/components/Home/Social.jsx
@@ -4,18 +4,21 @@ import { Icon } from '@iconify/react';
 const Social = () => {
   return (
     <div className="home__social">
-        <a href="https://www.linkedin.com/in/azimbek-toktosunov/" className="home__social-icon" target='_blank'>
-          <Icon icon="cib:linkedin" />
-        </a>
-        <a href="https://github.com/azimust" className="home__social-icon" target='_blank'>
-          <Icon icon="cib:github" />
-        </a>
-        <a href="https://vk.com/azimust" className="home__social-icon" target='_blank'>
-          <Icon icon="cib:vk" />
-        </a>
-        <a href="https://www.codewars.com/users/azimust" className="home__social-icon" target='_blank'>
-          <Icon icon="cib:codewars" />
-        </a>
+      <a href="https://www.linkedin.com/in/azimbek-toktosunov/" className="home__social-icon" target='_blank'>
+        <Icon icon="cib:linkedin" />
+      </a>
+      <a href="https://github.com/azimust" className="home__social-icon" target='_blank'>
+        <Icon icon="cib:github" />
+      </a>
+      <a href="https://vk.com/azimust" className="home__social-icon" target='_blank'>
+        <Icon icon="cib:vk" />
+      </a>
+      <a href="https://www.instagram.com/azimitto/" className="home__social-icon" target='_blank'>
+        <Icon icon="cib:instagram" />
+      </a>
+      <a href="https://www.codewars.com/users/azimust" className="home__social-icon" target='_blank'>
+        <Icon icon="cib:codewars" />
+      </a>
     </div>
   )
 }
