8dda8451e33060579c06b95ce246539afdb0192d
azimust <woody.azimbek@gmail.com>
2023-08-09 22:03:12 +0600

    add cv

--- src/components/Home/Social.jsx
+++ src/components/Home/Social.jsx
@@ -4,7 +4,7 @@ import { Icon } from '@iconify/react';
 const Social = () => {
   return (
     <div className="home__social">
-      <a href="https://www.linkedin.com/in/azimbek-toktosunov/" className="home__social-icon" target='_blank'>
+      <a href="https://www.linkedin.com/in/azimust/" className="home__social-icon" target='_blank'>
         <Icon icon="cib:linkedin" />
       </a>
       <a href="https://github.com/azimust" className="home__social-icon" target='_blank'>
